b635fbecfa79fa919649aeecf1c3085867e6cead
Doaa Mahely <dmahely@hotmail.com>
2021-07-02 23:07:42 +0400

    Style TodoInput component

--- src/TodoInput/TodoInput.js
+++ src/TodoInput/TodoInput.js
@@ -1,25 +1,34 @@
 import React, { useRef } from "react";
+import "./TodoInput.css";
 
 const TodoInput = ({ setTodos }) => {
   const textInputRef = useRef(null);
 
-  const handleSubmit = (e) => {
-    e.preventDefault();
-    // add the todo to state
-    const newTodo = { task: textInputRef.current.value, isCompleted: false };
-    setTodos((todos) => [...todos, newTodo]);
+  const handleOnKeyUp = (e) => {
+    if (e.keyCode === 13) {
+      e.preventDefault();
+      // add the todo to state
+      const newTodo = { task: textInputRef.current.value, isCompleted: false };
+      setTodos((todos) => [...todos, newTodo]);
 
-    // clear the input
-    textInputRef.current.value = "";
+      // clear the input
+      textInputRef.current.value = "";
+    }
+  };
+  const handleClick = (e) => {
+    // todo: update state and styling
   };
   return (
-    <form onSubmit={handleSubmit}>
+    <div className="TodoInput--container">
+      <button className="TodoInput--button" onClick={handleClick}></button>
       <input
         type="text"
         placeholder="Create a new todo..."
         ref={textInputRef}
+        className="TodoInput--input"
+        onKeyUp={handleOnKeyUp}
       />
-    </form>
+    </div>
   );
 };
 
